0005d4a03f2c98bb87dbe50903e38faf8c500c7f
Eakansh <eakansh.srivas125@gmail.com>
2022-07-18 18:03:23 +0530

    feat: added signup page

--- src/ComponentImporter.js
+++ src/ComponentImporter.js
@@ -1,6 +1,7 @@
 
 import Profile from "./Components/Profile/Profile";
 import Login from "./Components/Authentication/Login";
+import Signup from "./Components/Authentication/Signup";
 import Feed from "./Components/Feed/Feed";
 
-export {Profile, Login, Feed}
+export {Profile, Login, Signup, Feed}
