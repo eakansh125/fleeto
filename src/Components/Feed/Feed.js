a85f7d4902c80b8b74aa45640926b529c58a2ff3
Eakansh <eakansh.srivas125@gmail.com>
2022-07-18 17:38:08 +0530

    feat: added login page

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -13,7 +13,7 @@ function Feed() {
       <div className='profile-content-sidebar'>
         <div className='profile-content-sidebar-item'>
           <Link to="/" href='Feed.js' className='btn-text width-l margin-tb-s  br-m font-bold'>Home</Link>
-          <Link to="" className='btn-text width-l margin-tb-s br-m'>Explore</Link>
+          <Link to="/login" className='btn-text width-l margin-tb-s br-m'>Explore</Link>
           <Link to="" className='btn-text width-l margin-tb-s br-m'>Bookmarks</Link>
           <Link to="" className='btn-text width-l margin-tb-s br-m'>Notifications</Link>
           <Link to="/profile" className='btn-text width-l margin-t-0 br-m'>Profile</Link>
